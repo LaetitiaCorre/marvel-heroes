c4c3fb16ec9c436c469729a41f54a80671732869
LaetitiaCorre <laetitia.corre6@gmail.com>
2018-03-15 21:11:11 +0100

    First commit

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <h1>Marvel Heroes</h1>
+    <Character v-for="character in characters"  :key="character.id" :character="character"/>
+  </div>
+</template>
+
+<script>
+import Character from './components/Character'
+
+export default {
+  name: 'App',
+  components: {
+    Character
+  },
+
+  data () {
+    return {
+      characters: []
+    };
+  },
+
+  created() {
+    fetch('https://gateway.marvel.com/v1/public/characters?offset=100&apikey=82036cba2c097a8111714b4acc5d125a')
+    .then(res => res.json())
+    .then(json => json.data.results)
+    .then(characters => {
+      this.characters = characters;
+    });
+  }
+
+}
+</script>
+
+<style>
+
+body {
+  margin: 0;
+}
+
+#app {
+  background-color: rgb(61, 172, 144);
+  color: white;
+  margin: 0;
+}
+
+h1 {
+  font-family: 'Rammetto One', cursive;
+  font-size: 70px;
+  font-style: italic;
+  text-shadow: -1px 0 black, 0 1px black, 1px 0 black, 0 -1px black;
+  text-align: center;
+  padding: 30px 0;
+  margin: 0;
+}
+
+</style>
---
+++ src/components/Character.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="background">
+    <div class="containerHeroes">
+      <div class="firstDetail">
+        <div>
+          <h2>
+            {{character.name}}
+          </h2>
+        </div>
+
+        <div>
+          <img :src="character.thumbnail.path + '.' + character.thumbnail.extension" alt="">
+        </div>
+      </div>
+      
+    
+      <div v-if="showDetail">
+        <p>{{character.description}}, {{character.comics.available}}
+        </p>
+        <div v-for="item in character.comics.items.slice(0, 3)" :key="item.name" >
+          <p>{{item.name}}</p>
+        </div>
+      </div>
+      <button @click="showDetail=true" type="button">show</button>
+    </div>
+   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Character',
+
+  props: {
+    character: {
+      type: Object,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      showDetail: false
+    };
+  }
+}
+</script>
+
+
+<style scoped>
+
+.background {
+  background-color: rgb(61, 172, 144);
+  color: white;
+}
+
+.containerHeroes {
+  display: flex;
+  flex-direction: row;
+}
+
+.firstDetail {
+  display: flex;
+  flex-direction: row;
+}
+
+</style>
